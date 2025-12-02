3020934f166c0d5f0834b64b57c76c55994ba4e4
dipanshu-patidar <dipanshupatidar2002@gmail.com>
2025-12-02 16:56:15 +0530

    changes

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,6 +1,6 @@
 // const BaseUrl = 'https://accounting-prisma-production.up.railway.app/api/v1/';
-const BaseUrl = 'https://accounting-prisma-production.up.railway.app/api/v1/';
+// const BaseUrl = 'https://accounting-prisma-production.up.railway.app/api/v1/';
 // const BaseUrl = 'https://84kmwvvs-8080.inc1.devtunnels.ms/api/v1/';
-// const BaseUrl = 'https://84kmwvvs-8080.inc1.devtunnels.ms/api/v1/';
+const BaseUrl = 'https://t9s1vxch-8081.inc1.devtunnels.ms/api/v1/';
 
 export default BaseUrl;
