5743e3e6a0a363668efdd2d64f354f0d7723e46c
dipanshu-patidar <dipanshupatidar2002@gmail.com>
2025-12-09 19:19:23 +0530

    changes

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,5 +1,5 @@
 
-// const BaseUrl = 'https://t9s1vxch-8081.inc1.devtunnels.ms/api/v1/';
+// const BaseUrl = 'https://02x4fc84-8081.inc1.devtunnels.ms/api/v1/';
 const BaseUrl = 'https://accounting-arabic-production.up.railway.app/api/v1/';
 
 export default BaseUrl;
